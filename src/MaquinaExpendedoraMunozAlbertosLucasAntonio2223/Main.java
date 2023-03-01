07dd2ec6b5dfd810710b4994c7fae7ccc6c81427
Usuario <Usuario@SATC50PRO>
2023-03-01 11:33:00 +0100

    Proyecto MunozAlbertosLucasAntonio2223 comentado con JavaDoc

--- src/MaquinaExpendedoraMunozAlbertosLucasAntonio2223/Main.java
+++ src/MaquinaExpendedoraMunozAlbertosLucasAntonio2223/Main.java
@@ -5,19 +5,22 @@
 package MaquinaExpendedoraMunozAlbertosLucasAntonio2223;
 
 /**
+ * Programa ejemplo de analisis para la optimización de código.
  * XXX = MunozAlbertosLucasAntonio
- * @author efech
+ * @author Lucas Antonio Muñoz Albertos
+ * @version 1
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
+// instanciacion del objeto
               MáquinaExpendedora snack;
        
-        
+        // creación del objeto snack
         snack=new MáquinaExpendedora(13,2,5);
+
+        // métodos que se utilizan
         sube1_PrecioMunozAlbertosLucasAntonio2223(snack);
         
         sube2_PrecioMunozAlbertosLucasAntonio2223(snack);
@@ -30,36 +33,10 @@ public class Main {
         
     }
 
-    private static void obtener3_UnidadesMunozAlbertosLucasAntonio2223(MáquinaExpendedora snack) {
-        /*Vamos a intentar sacar 12 snack, y tengo 50 euros.
-        Como no hay unidades suficientes en la máquina saltará
-        una excepción y no se modificará el nº de unidades de la tienda*/
-        try {
-            System.out.println("3º.- Vamos a sacar snacks (queremos 12 y tenemos 50 €)");
-            snack.sacarsnack(12, 50, "pegatina");
-            System.out.println("Operación realizada, ahora tiene "+snack.obtenerUnidades()+" unidades en la máquina");
-        } catch (Exception e) {
-            System.out.println("Error al sacar snack,porque no hay suficientes unidades");
-            System.out.println("El nº de unidades sigue siendo: "+snack.obtenerUnidades());
-            
-        }
-    }
-
-    private static void obtener2_UnidadesMunozAlbertosLucasAntonio2223(MáquinaExpendedora snack) {
-        /*Vamos a intentar sacar 2 snack, y tengo 9 euros.
-        Ahora se actualizará el número de snack que tenemos en
-        la máquina*/
-        try {
-            System.out.println("2º.- Vamos a sacar snacks (queremos 2 y tenemos 9 €)");
-            snack.sacarsnack(2, 9, "pegatina");
-            System.out.println("Operación realizada, ahora tiene "+snack.obtenerUnidades()+" unidades en la máquina");
-        } catch (Exception e) {
-            System.out.println("Error al sacar snack,porque no hay suficientes dinero");
-            System.out.println("El nº de unidades sigue siendo: "+snack.obtenerUnidades());
-            
-        }
-    }
-
+    
+/**
+* método privado para obtener las unidades de snack y realizar prueba
+*/
     private static void obtener1_UnidadesMunozAlbertosLucasAntonio2223(MáquinaExpendedora snack) {
         /*Vamos a intentar sacar 2 snack, y tengo 5 euros.
         Como no hay dinero suficiente saltará
@@ -73,20 +50,45 @@ public class Main {
             System.out.println("El nº de unidades sigue siendo: "+snack.obtenerUnidades());
             
         }
-    }
 
-    private static void sube2_PrecioMunozAlbertosLucasAntonio2223(MáquinaExpendedora snack) {
-        /*Vamos a aumentar el precio 3€, como supera el precio máximo establecido la operación
-        no tendrá éxito y se mantendrá el precio anterior*/
+   
+    }
+      /**
+* método privado para obtener las unidades de snack y realizar prueba
+*/ 
+    private static void obtener2_UnidadesMunozAlbertosLucasAntonio2223(MáquinaExpendedora snack) {
         try {
-            System.out.println("Vamos a aumentar el precio de venta de un snack por encima del precio máximo");
-            snack.aumentarPrecio(3);
-            System.out.println("El precio actual es "+snack.obtenerPrecio()+" €");
+            System.out.println("2º.- Vamos a sacar snacks (queremos 2 y tenemos 9 €)");
+            snack.sacarsnack(2, 9, "pegatina");
+            System.out.println("Operación realizada, ahora tiene "+snack.obtenerUnidades()+" unidades en la máquina");
         } catch (Exception e) {
-            System.out.println("Error al aumentar el precio porque se supera el precio máximo de 5 €");
+            System.out.println("Error al sacar snack,porque no hay suficientes dinero");
+            System.out.println("El nº de unidades sigue siendo: "+snack.obtenerUnidades());
+            
+        }
+    }
+/**
+* método privado para obtener las unidades de snack y realizar prueba
+*/
+     private static void obtener3_UnidadesMunozAlbertosLucasAntonio2223(MáquinaExpendedora snack) {
+        
+        try {
+            System.out.println("3º.- Vamos a sacar snacks (queremos 12 y tenemos 50 €)");
+            snack.sacarsnack(12, 50, "pegatina");
+            System.out.println("Operación realizada, ahora tiene "+snack.obtenerUnidades()+" unidades en la máquina");
+        } catch (Exception e) {
+            System.out.println("Error al sacar snack,porque no hay suficientes unidades");
+            System.out.println("El nº de unidades sigue siendo: "+snack.obtenerUnidades());
+            
         }
     }
 
+    
+    
+/**
+* método privado para subir precio y realizar prueba
+*/
+    
     private static void sube1_PrecioMunozAlbertosLucasAntonio2223(MáquinaExpendedora snack) {
         /*Vamos a aumentar el precio 1€, como no supera el precio máximo establecido la operación
         tendrá éxito y el nuevo precio será de 3€*/
@@ -98,5 +100,19 @@ public class Main {
             System.out.println("Error al aumentar el precio");
         }
     }
-    
+     
+/**
+* método privado para subir precio y realizar prueba
+*/
+    private static void sube2_PrecioMunozAlbertosLucasAntonio2223(MáquinaExpendedora snack) {
+        /*Vamos a aumentar el precio 3€, como supera el precio máximo establecido la operación
+        no tendrá éxito y se mantendrá el precio anterior*/
+        try {
+            System.out.println("Vamos a aumentar el precio de venta de un snack por encima del precio máximo");
+            snack.aumentarPrecio(3);
+            System.out.println("El precio actual es "+snack.obtenerPrecio()+" €");
+        } catch (Exception e) {
+            System.out.println("Error al aumentar el precio porque se supera el precio máximo de 5 €");
+        }
+    }
 }
--- src/MaquinaExpendedoraMunozAlbertosLucasAntonio2223/MáquinaExpendedora.java
+++ src/MaquinaExpendedoraMunozAlbertosLucasAntonio2223/MáquinaExpendedora.java
@@ -5,14 +5,36 @@
 package MaquinaExpendedoraMunozAlbertosLucasAntonio2223;
 
 /**
- *
- * @author efech
+ *Clase MáquinaExpendedora. Con esta clase se crea objetos maquinaexpendedora con  los que se realiza las prueba.
+ * @author Lucas Antonio Muñoz Albertos
  */
 
 public class MáquinaExpendedora {
 
+// Consideramos máquina snack precio único
+    private int unidades;//número de snacks disponbles en la máquina expendora 
+    private double precio;//precio actual de un snack
+    private double precio_maxMunozAlbertosLucasAntonio2223;//precio máximo que puede tener un snack
+
+/**Constructor por defecto. Método constructor sin parámetros**/
+    public MáquinaExpendedora() {
+    
+    }
+    
+    
+     /**Constructor por parámetros. Se crea un objeto MáquinaExpendedora con los siguientes atributos
+@param unidades número de elementos snack que tiene la máquina
+@param precio precio del snack
+@param precio_max precio máximo por unidad de snack**/
+    public MáquinaExpendedora(int unidades, double precio, double precio_max) {
+        this.unidades = unidades;
+        this.precio = precio;
+        this.precio_maxMunozAlbertosLucasAntonio2223 = precio_max;
+    }
+
     /**
-     * @return the precio
+    * metodo get que devuelve el precio del snack.
+     * @return devuelve valor de la variable precio
      */
     public double getPrecio() {
         return precio;
@@ -25,8 +47,10 @@ public class MáquinaExpendedora {
         this.precio = precio;
     }
 
+    
     /**
-     * @return the precio_maxMunozAlbertosLucasAntonio2223
+    * metodo get que devuelve el precio maximo del snack.
+     * @return devuelve valor de la variable precio maximo del snack
      */
     public double getPrecio_maxMunozAlbertosLucasAntonio2223() {
         return precio_maxMunozAlbertosLucasAntonio2223;
@@ -38,41 +62,36 @@ public class MáquinaExpendedora {
     public void setPrecio_maxMunozAlbertosLucasAntonio2223(double precio_maxMunozAlbertosLucasAntonio2223) {
         this.precio_maxMunozAlbertosLucasAntonio2223 = precio_maxMunozAlbertosLucasAntonio2223;
     }
-// Consideramos máquina snack precio único
-    private int unidades;//número de snacks disponbles en la máquina expendora 
-    private double precio;//precio actual de un snack
-    private double precio_maxMunozAlbertosLucasAntonio2223;//precio máximo que puede tener un snack
 
-    /*Constructor por defecto*/
-    public MáquinaExpendedora() {
-    
-    }
+
     
     
-     /*Constructor por parámetros*/
-    public MáquinaExpendedora(int unidades, double precio, double precio_max) {
-        this.unidades = unidades;
-        this.precio = precio;
-        this.precio_maxMunozAlbertosLucasAntonio2223 = precio_max;
-    }
-    
-    /*Método que devuelve el número de snack que tiene la máquina*/
+    /**Método que devuelve el número de snack que tiene la máquina
+* @return las unidades de snack**/
     public int obtenerUnidades() {
         return this.unidades;
     }
 
-    /*Método que devuelve el precio que tiene cada snack*/
+    /**Método que devuelve el precio que tiene cada snack
+    * @return el precio del snack
+**/
     public double obtenerPrecio() {
         return this.getPrecio();
     }
     
-     /*Método que permite modificar el número de snack dsiponbles de la máquina*/
+     /**Método que permite modificar el número de snack dsiponbles de la máquina 
+*@param unidades numero de unidades que tienen la maquina expendedora
+**/
     public void modificarUnidades(int unidades) {
         this.unidades = unidades;
     }
     
-     /*Método que permite aumentar el precio de venta, suma al precio actual 
-    el aumento que se indica siempre que no se sobrepase el precio máximo de venta*/
+     /**
+*Método que permite aumentar el precio de venta, suma al precio actual 
+    *el aumento que se indica siempre que no se sobrepase el precio máximo de venta
+*@param aumento cantidad de unidades que deseamos aumentar el precio.
+*@throws Exception excepcion cuando el aumento es negativo
+**/
     public void aumentarPrecio(double aumento) throws Exception {
         if (aumento <= 0) {
             throw new Exception("El aumento debe ser positivo");
@@ -85,8 +104,15 @@ public class MáquinaExpendedora {
     
        
     
-    /*Método que permite obtener un snack si se tiene suficiente dinero y hay 
-suficientes unidades en la máquina*/
+    /**
+*Método que permite obtener un snack si se tiene suficiente dinero y hay 
+*suficientes unidades en la máquina
+* @param unidades numero de unidades de snack que vamos a sacar.
+* @param dinero dinero que se introduce.
+* @param regaloMunozAlbertosLucasAntonio regalo asociado al sacar el snack.
+@throws Exception excepcion cuando fallo al introducir un valor de dinero negativo o el producto total de la compra es mayor al dinero disponible, la cantidad de unidades es negativa o no hay suficientes
+
+**/
     public void sacarsnack(int unidades, double dinero, String regaloMunozAlbertosLucasAntonio) throws Exception {
         if (dinero <= 0) {
             throw new Exception("No puede introducirse un valor negativo para el dinero");
